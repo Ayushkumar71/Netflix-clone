2047a381b7a201a521fb24d36449ba201014c5b1
Ayush Kumar <ayush@Ayushs-MacBook-Air.local>
2023-08-24 17:06:22 +0530

    Template ready

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import HomeScreen from './pages/HomeScreen';
+import './stylesheets/App.css';
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+        <h1>Neflix Clone</h1>
+      </div>
+      <br />
+      <HomeScreen />
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7412hcYDOshygUkmXyorfHX3NYn7W454",
+  authDomain: "netflix-clone-c0dbb.firebaseapp.com",
+  projectId: "netflix-clone-c0dbb",
+  storageBucket: "netflix-clone-c0dbb.appspot.com",
+  messagingSenderId: "417648143386",
+  appId: "1:417648143386:web:15255be924ab294b4535a3",
+};
